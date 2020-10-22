5979c19c5a1bf197e4874e7e2363bc2a2b0df568
alaa140101 <alaa140011@gmail.com>
2020-10-22 09:46:53 +0300

    Computer Selection

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ const computerLizard = document.getElementById('computerLizard');
 const computerSpock = document.getElementById('computerSpock');
 
 const allGameIcons = document.querySelectorAll('.far');
+let computerChoice = '';
 
+// Reset all 'selected' icons
 function resetIcons() {
   allGameIcons.forEach(icon => icon.classList.remove('selected'));
 }
@@ -30,9 +32,61 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+// Random computer choice
+function computerRandomChoice(){
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber < 0.2) {
+    computerChoice = 'rock';
+  }else if(computerChoiceNumber < 0.4) {
+    computerChoice = 'paper';
+  }else if(computerChoiceNumber < 0.6) {
+    computerChoice = 'scissors';
+  }else if(computerChoiceNumber < 0.8) {
+    computerChoice = 'lizard';
+  }else{
+    computerChoice = 'spock';
+  }
+}
+
+// Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+  
+    default:
+      break;
+  }
+}
+
+// Call functions to process turn
+function checkResult() {
+  computerRandomChoice();
+  displayComputerChoice();
+  resetIcons();
+}
+
 // Passing player selection value and styling icons
 function select(playerChoice) {
-  resetIcons();
+  checkResult();
   // Add 'selected' styleing & playerChoice
   switch (playerChoice) {
     case 'rock':
